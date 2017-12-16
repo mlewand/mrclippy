91cea37ca05f3192e308c0f15192b4dd8b852700
Marek Lewandowski <marek@mlewandowski.com>
2017-12-16 01:37:42 +0100

    Added ClipboardSnapshot test suite.

---
+++ tests/ClipboardSnapshot.test.js
@@ -0,0 +1,31 @@
+// This will mock  win-clipboard.
+require( './mock/ClipboardSnapshotMock' );
+
+const ClipboardSnapshot = require( '../src/ClipboardSnapshot' ),
+	chai = require( 'chai' ),
+	expect = chai.expect;
+
+chai.use( require( 'chai-as-promised' ) );
+
+describe( 'ClipboardSnapshot', () => {
+	describe( 'createFromData', () => {
+		let sampleData = {
+				meta: {
+					os: 'win10',
+					label: 'snapshot-mock',
+					format: '1',
+					appVersion: '0.0.1'
+				},
+				data: {
+					'HTML Format': '123',
+					CF_UNICODETEXT: 'abc',
+					Binary: Buffer.from( [ 64, 64, 64 ] )
+				}
+			},
+			ret = ClipboardSnapshot.createFromData( sampleData );
+
+		expect( ret ).to.be.instanceOf( ClipboardSnapshot );
+		expect( ret.getValue( 'CF_UNICODETEXT' ) ).to.be.equal( 'abc' );
+		expect( ret.getValue( 'Binary' ) ).to.be.eql( Buffer.from( [ 64, 64, 64 ] ) );
+	} );
+} );
