fc1af41d9d69876c903b1382eb60bb98c47fd36a
Marek Lewandowski <marek@mlewandowski.com>
2017-12-18 00:03:41 +0100

    Added some API docs.

--- src/OsEnvironment.js
+++ src/OsEnvironment.js
@@ -6,14 +6,34 @@ const os = require( 'os' );
  */
 class OsEnvironment {
 	/**
+	 * Creates a new OS environment object.
+	 *
+	 * Note that the objects are immutable.
 	 *
 	 * @param {string} name Platform name as returned by node's `os.platform()`.
 	 * @param {Mixed} release Version as returned by node's `os.release()`.
 	 */
 	constructor( name, release ) {
+		/**
+		 * Name of the OS, e.g. `'win32'`, `'linux'`, `'darwin'`, see
+		 * {@link https://nodejs.org/api/os.html#os_os_platform node platforms list} for more.
+		 *
+		 * @readonly
+		 * @property {string}
+		 */
 		this.name = name;
+
+		/**
+		 * Release identifier of the OS, as returned by
+		 * {@link https://nodejs.org/api/os.html#os_os_release node `os.release()`} function.
+		 */
 		this.release = release;
 
+		/**
+		 * OS major version, e.g. `'7'` in case of Windows 7.
+		 *
+		 * @property {string}
+		 */
 		this.version = this._getVersion( release );
 
 		Object.freeze( this );
