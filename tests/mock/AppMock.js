641671c461a379d74be2764b3a25dfd697b3be3a
Marek Lewandowski <marek@mlewandowski.com>
2017-12-21 00:59:26 +0100

    Fixed rejected promise thrown in the background by localForage.

--- tests/mock/AppMock.js
+++ tests/mock/AppMock.js
@@ -1,9 +1,17 @@
-const Storage = require( '../../src/Storage' );
-
 class AppMock {
 	constructor() {
 		this.config = require( '../_fixtures/config.json' );
-		this.storage = new Storage();
+
+		// Can't use real localforage instance, or it will throw: "UnhandledPromiseRejectionWarning: Unhandled promise
+		// rejection (rejection id: 10): Error: No available storage method found." error.
+		this.storage = {
+			snapshots: {
+				keys: () => null,
+				setItem: () => null,
+				getItem: () => null,
+				removeItem: () => null
+			}
+		};
 	}
 }
 
